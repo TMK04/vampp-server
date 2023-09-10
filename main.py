a84f20e9603616f648e7c42b1ed22ebb28b73899
TMK04 <floer.learner.01@gmail.com>
2023-09-11 03:24:15 +0800

    write wav output to temp

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def compressCommand(mp4_name):
   ]
 
 
-def audioCommand(mp4_name):
-  return ["ffmpeg", "-i", mp4_name, "-vn", "-f", "wav", "-"]
+def audioCommand(mp4_name, wav_name):
+  return ["ffmpeg", "-i", mp4_name, "-vn", wav_name]
 
 
 @app.get("/")
@@ -33,31 +33,31 @@ async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   # Convert to bytes-like object
   video_bytes = await file.read()
   # Create temp file
-  with tempfile.NamedTemporaryFile(suffix=".mp4") as temp:
-    temp.write(video_bytes)
-    temp.flush()
+  with tempfile.NamedTemporaryFile(suffix=".mp4") as mp4_temp:
+    mp4_temp.write(video_bytes)
+    mp4_temp.flush()
     # Compress
-    compress_process = subprocess.Popen(compressCommand(temp.name), stdout=subprocess.PIPE)
+    compress_process = subprocess.Popen(compressCommand(mp4_temp.name), stdout=subprocess.PIPE)
     video_bytes, err = compress_process.communicate()
     if err:
       print(err)
       return "error"
-    print(video_bytes)
-    # Overwrite temp file
-    temp.seek(0)
-    temp.write(video_bytes)
-    temp.flush()
+    print(type(video_bytes))
+    video_key = os.path.join("og", basename + ".mp4")
+    s3_client.upload_fileobj(mp4_temp, "vampp", video_key)
+    # Overwrite mp4_temp file
+    mp4_temp.seek(0)
+    mp4_temp.write(video_bytes)
+    mp4_temp.flush()
     # Extract audio
-    audio_process = subprocess.Popen(audioCommand(temp.name), stdout=subprocess.PIPE)
-    audio_bytes, err = audio_process.communicate()
-    if err:
-      print(err)
-      return "error"
-    print(audio_bytes)
-  # Upload to S3
-  video_key = os.path.join(topic, basename + ".mp4")
-  audio_key = os.path.join("audio", topic, basename + ".wav")
-  s3_client.put_object(Bucket="vampp", Key=video_key, Body=video_bytes)
-  s3_client.put_object(Bucket="vampp", Key=audio_key, Body=audio_bytes)
+    with tempfile.NamedTemporaryFile(suffix=".wav") as wav_temp:
+      audio_process = subprocess.Popen(audioCommand(mp4_temp.name, wav_temp.name))
+      audio_bytes, err = audio_process.communicate()
+      if err:
+        print(err)
+        return "error"
+      print(type(audio_bytes))
+      audio_key = os.path.join("audio/og", basename + ".wav")
+      s3_client.upload_fileobj(wav_temp, "vampp", audio_key)
 
   return "ok"

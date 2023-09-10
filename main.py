6083401f7fea251e3d3360d33352facc3bdae803
TMK04 <floer.learner.01@gmail.com>
2023-09-11 04:43:05 +0800

    use tempdirectory context manager

--- main.py
+++ main.py
@@ -31,25 +31,21 @@ async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   mp4_name = file.filename
   wav_name = file.filename.replace(".mp4", ".wav")
 
-  temp_dir = tempfile.mkdtemp()
-  tempfile_name = os.path.join(temp_dir.name, "temp.mp4")
-  # Save file to temp
-  with open(tempfile_name, "wb") as f:
-    f.write(await file.read())
+  with tempfile.TemporaryDirectory() as tempdir_name:
+    tempfile_name = os.path.join(tempdir_name, "temp.mp4")
+    # Save file to temp
+    with open(tempfile_name, "wb") as f:
+      f.write(await file.read())
 
-  # Compress
-  tempmp4_name = os.path.join(temp_dir.name, mp4_name)
-  compressVideo(tempfile_name, tempmp4_name)
-  video_key = os.path.join("og", mp4_name)
-  print(video_key)
-  s3_client.upload_file(tempmp4_name, "vampp", video_key)
-  # Extract audio
-  tempwav_name = os.path.join(temp_dir.name, wav_name)
-  extractAudio(tempmp4_name, tempwav_name)
-  audio_key = os.path.join("audio/og", wav_name)
-  print(audio_key)
-  s3_client.upload_file(tempwav_name, "vampp", audio_key)
-
-  temp_dir.cleanup()
+    # Compress
+    tempmp4_name = os.path.join(tempdir_name, mp4_name)
+    compressVideo(tempfile_name, tempmp4_name)
+    video_key = os.path.join("og", mp4_name)
+    s3_client.upload_file(tempmp4_name, "vampp", video_key)
+    # Extract audio
+    tempwav_name = os.path.join(tempdir_name, wav_name)
+    extractAudio(tempmp4_name, tempwav_name)
+    audio_key = os.path.join("audio/og", wav_name)
+    s3_client.upload_file(tempwav_name, "vampp", audio_key)
 
   return "ok"

4fc510c58b7d850df7e6f445d3e711ffafc29b4a
TMK04 <floer.learner.01@gmail.com>
2023-09-11 02:24:41 +0800

    print types of bytes
    are they really bytes?

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import boto3
 from fastapi import FastAPI, UploadFile, Form
 import os
 import subprocess
+import tempfile
 
 app = FastAPI()
 s3_client = boto3.client('s3')
 
-compress_command = [
-    "ffmpeg", "-hwaccel", "cuda", "-vf", "fps=1", "-c:v", "libx265", "-crf", "28", "-acodec",
-    "pcm_s16le", "-ar", "16000", "-ac", "1"
-]
-audio_command = [
-    "ffmpeg",
-    "-vn",
-    "--audio-format",
-    "wav",
-]
+
+def compressCommand(tempfile_name):
+  return [
+      "ffmpeg", "-i", tempfile_name, "-vf", "fps=1", "-c:v", "libx265", "-crf", "28", "-acodec",
+      "pcm_s16le", "-ar", "16000", "-ac", "1", "-"
+  ]
+
+
+def audioCommand(tempfile_name):
+  return ["ffmpeg", "-i", "-", "-vn", "-f", "wav", "--", tempfile_name]
 
 
 @app.get("/")
@@ -31,22 +32,28 @@ async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   print("Topic:", topic)
   # Convert to bytes-like object
   video_bytes = await file.read()
-  # Compress
-  compress_process = subprocess.Popen(compress_command, stdin=subprocess.PIPE)
-  audio_process = subprocess.Popen(audio_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-  video_bytes, err = compress_process.communicate(video_bytes)
-  if err:
-    print(err)
-    return "error"
-  # Extract audio
-  audio_bytes, err = audio_process.communicate(video_bytes)
-  if err:
-    print(err)
-    return "error"
+  # Create temp file
+  with tempfile.NamedTemporaryFile(suffix=".mp4") as temp:
+    temp.write(video_bytes)
+    temp.flush()
+    # Compress
+    compress_process = subprocess.Popen(compressCommand(temp.name), stdout=subprocess.PIPE)
+    audio_process = subprocess.Popen(audioCommand(temp.name), stdin=subprocess.PIPE)
+    video_bytes, err = compress_process.communicate()
+    if err:
+      print(err)
+      return "error"
+    # Extract audio
+    audio_bytes, err = audio_process.communicate()
+    if err:
+      print(err)
+      return "error"
+  print(type(video_bytes), type(audio_bytes))
+  print(video_bytes)
+  print(audio_bytes)
   # Upload to S3
-  s3_client.put_object(Bucket="vampp", Key=os.path.join("og", basename + ".mp4"), Body=video_bytes)
-  s3_client.put_object(Bucket="vampp",
-                       Key=os.path.join("audio/og", basename + ".wav"),
-                       Body=audio_bytes)
+  video_key = os.path.join(topic, basename + ".mp4")
+  audio_key = os.path.join("audio", topic, basename + ".wav")
+  s3_client.put_object(Bucket="vampp", Key=video_key, Body=video_bytes)
 
   return "ok"

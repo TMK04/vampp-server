ff7ba4eda92763a848d36c639c4c8ba6ef786bb1
TMK04 <floer.learner.01@gmail.com>
2023-09-11 01:34:26 +0800

    pipe output & add audio process

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import boto3
 from fastapi import FastAPI, UploadFile, Form
 import ffmpeg
 import os
+from uuid import uuid4
 
 app = FastAPI()
 s3_client = boto3.client('s3')
@@ -12,14 +13,16 @@ def read_root():
   return {"Hello": "World"}
 
 
-process = (ffmpeg.input("pipe:").output("out.mp4",
-                                        hwaccel="cuda",
-                                        vf="fps=1",
-                                        vcodec="libx265",
-                                        crf=28,
-                                        s='{}x{}'.format(
-                                            1280,
-                                            720)).overwrite_output().run_async(pipe_stdin=True))
+compress_process = (ffmpeg.input("pipe:").output("pipe:",
+                                                 hwaccel="cuda",
+                                                 vf="fps=1",
+                                                 vcodec="libx265",
+                                                 crf=28,
+                                                 acodec="pcm_s16le",
+                                                 ar=16000,
+                                                 ac=1).run_async(pipe_stdin=True, pipe_stdout=True))
+audio_process = (ffmpeg.input("pipe:").output("pipe:", format="wav",
+                                              vn=1).run_async(pipe_stdin=True, pipe_stdout=True))
 
 
 @app.post("/")
@@ -27,10 +30,19 @@ async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   # Log
   print("File:", file.filename)
   print("Topic:", topic)
+  basename = uuid4().hex
   # Convert to bytes-like object
-  file_bytes = await file.read()
-  await process.communicate(input=file_bytes)[0]
+  video_bytes = await file.read()
+  # Compress
+  video_bytes = await compress_process.communicate(input=video_bytes)[0]
+  # Extract audio
+  audio_bytes = await audio_process.communicate(input=video_bytes)[0]
   # Upload to S3
-  Key = os.path.join("og", file.filename)
-  s3_client.put_object(Bucket="vampp", Key=Key, Body=file_bytes)
+  await s3_client.put_object(Bucket="vampp",
+                             Key=os.path.join("og", basename + ".mp4"),
+                             Body=video_bytes)
+  await s3_client.put_object(Bucket="vampp",
+                             Key=os.path.join("audio/og", basename + ".wav"),
+                             Body=audio_bytes)
+
   return "ok"

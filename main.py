202534f18cec0346274505bd12041532fc23f7d0
TMK04 <floer.learner.01@gmail.com>
2023-09-11 01:10:04 +0800

    reimplement ffmpeg process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 from fastapi import FastAPI, UploadFile, Form
+import ffmpeg
 import os
 
 app = FastAPI()
@@ -11,14 +12,18 @@ def read_root():
   return {"Hello": "World"}
 
 
+process = (ffmpeg.input("pipe:").output("out.mp4", s='{}x{}'.format(
+    1280, 720)).overwrite_output().run_async(pipe_stdin=True))
+
+
 @app.post("/")
 async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   # Log
-  print("Video:", file)
+  print("File:", file.filename)
   print("Topic:", topic)
   # Convert to bytes-like object
   file_bytes = await file.read()
-  print(file_bytes)
+  file_bytes = process.communicate(input=file_bytes)[0]
   # Upload to S3
   Key = os.path.join("og", file.filename)
   s3_client.put_object(Bucket="vampp", Key=Key, Body=file_bytes)

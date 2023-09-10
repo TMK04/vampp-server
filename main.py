e07d4a8ce438c62bd4cc6c1a4f0c71ed083e4777
TMK04 <floer.learner.01@gmail.com>
2023-09-11 00:20:00 +0800

    add video handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form
 
 app = FastAPI()
 
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
+
+
+@app.post("/")
+async def receive_video(video: bytes = File(...), topic: str = Form(...)):
+  # Log
+  print("Video size:", len(video))
+  print("String data:", string_data)
+  return {"video_size": len(video), "string_data": string_data}

ef899b4fdfdd73090e312d330ac409f3a9b42883
TMK04 <floer.learner.01@gmail.com>
2023-09-11 01:52:25 +0800

    add error handling

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import boto3
 from fastapi import FastAPI, UploadFile, Form
 import ffmpeg
 import os
-from uuid import uuid4
 
 app = FastAPI()
 s3_client = boto3.client('s3')
@@ -14,13 +13,15 @@ def read_root():
 
 
 compress_process = (ffmpeg.input("pipe:").output("pipe:",
-                                                 hwaccel="cuda",
                                                  vf="fps=1",
                                                  vcodec="libx265",
                                                  crf=28,
                                                  acodec="pcm_s16le",
                                                  ar=16000,
-                                                 ac=1).run_async(pipe_stdin=True, pipe_stdout=True))
+                                                 ac=1,
+                                                 s='{}x{}'.format(1280,
+                                                                  720)).run_async(pipe_stdin=True,
+                                                                                  pipe_stdout=True))
 audio_process = (ffmpeg.input("pipe:").output("pipe:", format="wav",
                                               vn=1).run_async(pipe_stdin=True, pipe_stdout=True))
 
@@ -28,15 +29,21 @@ audio_process = (ffmpeg.input("pipe:").output("pipe:", format="wav",
 @app.post("/")
 async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   # Log
-  print("File:", file.filename)
+  basename = file.filename.replace(".mp4", "")
+  print("File:", basename)
   print("Topic:", topic)
-  basename = uuid4().hex
   # Convert to bytes-like object
   video_bytes = await file.read()
   # Compress
-  video_bytes = compress_process.communicate(input=video_bytes)[0]
+  video_bytes, err = compress_process.communicate(input=video_bytes)
+  if err:
+    print(err)
+    return "error"
   # Extract audio
-  audio_bytes = audio_process.communicate(input=video_bytes)[0]
+  audio_bytes, err = audio_process.communicate(input=video_bytes)
+  if err:
+    print(err)
+    return "error"
   # Upload to S3
   s3_client.put_object(Bucket="vampp", Key=os.path.join("og", basename + ".mp4"), Body=video_bytes)
   s3_client.put_object(Bucket="vampp",

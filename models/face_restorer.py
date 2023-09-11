7a7aa8fd65a09f5f9e9845d28ec6b0c4b4c5e33e
TMK04 <floer.learner.01@gmail.com>
2023-09-11 20:50:44 +0800

    fix GFPGAN output dir

--- models/face_restorer.py
+++ models/face_restorer.py
@@ -5,11 +5,12 @@ MODEL_FR_PATH = os.environ.get("MODEL_FR_PATH")
 if MODEL_FR_PATH is None:
   raise Exception("MODEL_PL_PATH is not set")
 
-OUTPUT_PATH = os.path.join(MODEL_FR_PATH, "nly_center_face")
+MODEL_FR_OUTPUT_DIR = os.path.join(MODEL_FR_PATH, "nly_center_face")
 
 
-def restoreFaces(input_dir, output_dir):
+def restoreFaces(input_dir):
   subprocess.run([
-      "python", "inference_gfpgan.py", "-i", input_dir, "-v", "1.4", "-s", "2", "-only_center_face"
+      "python", "inference_gfpgan.py", "-i", input_dir, "-o", MODEL_FR_OUTPUT_DIR, "-v", "1.4",
+      "-s", "2", "-only_center_face"
   ],
                  cwd=MODEL_FR_PATH)

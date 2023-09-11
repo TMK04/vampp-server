d86134fea1aed887a47f421b038ce771397b4990
TMK04 <floer.learner.01@gmail.com>
2023-09-11 22:37:16 +0800

    remove only_center_face

--- models/face_restorer.py
+++ models/face_restorer.py
@@ -8,8 +8,15 @@ if MODEL_FR_PATH is None:
 
 def restoreFaces(input_dir, output_dir):
   subprocess.run([
-      "python", "inference_gfpgan.py", "-i",
-      os.path.abspath(input_dir), "-o",
-      os.path.abspath(output_dir), "-v", "1.4", "-s", "1", "-only_center_face"
+      "python",
+      "inference_gfpgan.py",
+      "-i",
+      os.path.abspath(input_dir),
+      "-o",
+      os.path.abspath(output_dir),
+      "-v",
+      "1.4",
+      "-s",
+      "1",
   ],
                  cwd=MODEL_FR_PATH)

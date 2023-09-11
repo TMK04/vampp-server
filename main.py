04f95835a80be1366e700099eee59fc1b8568de8
TMK04 <floer.learner.01@gmail.com>
2023-09-11 20:54:48 +0800

    unfix GFPGAN output dir

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2
 from cv_helpers import extractFrames, resizeToLocalize
 from fastapi import FastAPI, UploadFile, Form
 from ffmpeg_commands import compressVideo, extractAudio
-from models.face_restorer import MODEL_FR_OUTPUT_DIR, restoreFaces
+from models.face_restorer import restoreFaces
 from models.presenter_localizer import calculatePresenterXYXYN, localizePresenter
 import os
 import pandas as pd
@@ -137,8 +137,8 @@ async def receive_video(file: UploadFile = Form(...), topic: str = Form(...)):
   temp_restored_dir_name = tempName(["frame", "restored"])
   Path(temp_restored_dir_name).mkdir()
   print(os.listdir(temp_localized_dir_name))
-  restoreFaces(temp_localized_dir_name)
-  print(os.listdir(MODEL_FR_OUTPUT_DIR))
+  restoreFaces(temp_localized_dir_name, temp_restored_dir_name)
+  print(os.listdir(temp_restored_dir_name))
 
   shutil.rmtree(temp_dir_name, ignore_errors=True)
   return "ok"
--- models/face_restorer.py
+++ models/face_restorer.py
@@ -5,12 +5,8 @@ MODEL_FR_PATH = os.environ.get("MODEL_FR_PATH")
 if MODEL_FR_PATH is None:
   raise Exception("MODEL_PL_PATH is not set")
 
-MODEL_FR_OUTPUT_DIR = os.path.join(MODEL_FR_PATH, "nly_center_face")
 
-
-def restoreFaces(input_dir):
-  subprocess.run([
-      "python", "inference_gfpgan.py", "-i", input_dir, "-o", MODEL_FR_OUTPUT_DIR, "-v", "1.4",
-      "-s", "2", "-only_center_face"
-  ],
-                 cwd=MODEL_FR_PATH)
+def restoreFaces(input_dir, output_dir):
+  subprocess.run(
+      ["python", "inference_gfpgan.py", "-i", input_dir, "-o", output_dir, "-v", "1.4", "-s", "2"],
+      cwd=MODEL_FR_PATH)

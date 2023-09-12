fd677a2ac649ca056744a4bffe547bc70cf2571e
TMK04 <floer.learner.01@gmail.com>
2023-09-13 02:33:05 +0800

    add concurrency

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from typing import Union
 
 from audio import transcribe, splitAudio, splitAudioBatch
 from aws import AWS_DYNAMO_TABLE, AWS_S3_BUCKET, USE_AWS, dynamo_client, s3_client
-import cv2
+import concurrent.futures
 from config import FRAME_ATTIRE_MASK
 from cv_helpers import extractFrames, processRestoredFrames, resizeToLocalize
+import cv2
 from fastapi import FastAPI, HTTPException, UploadFile, Form
 from models.components import device, infer, toTensor
 from models.face_restorer import restoreFaces
@@ -91,6 +92,7 @@ async def receive_video(topic: str = Form(...), file: Union[UploadFile, str] = F
   wav_arg_ls = ["audio", "og.wav"]
   temp_wav_name = tempName(wav_arg_ls)
   extractAudio(temp_mp4_name, temp_wav_name)
+
   # if USE_AWS:
   #   audio_key = s3Key(wav_arg_ls)
   #   s3_client.upload_file(temp_wav_name, AWS_S3_BUCKET, audio_key)
@@ -100,7 +102,6 @@ async def receive_video(topic: str = Form(...), file: Union[UploadFile, str] = F
   Path(temp_localized_dir_name).mkdir()
 
   def localizeFrames():
-    localized_frame_ls = []
     # if USE_AWS:
     #   xyxyn_df = {key: [] for key in ["i", "x1", "y1", "x2", "y2"]}
     for batch in extractFrames(temp_mp4_name):
@@ -125,7 +126,6 @@ async def receive_video(topic: str = Form(...), file: Union[UploadFile, str] = F
         i_jpg = f"{i}.jpg"
         temp_localized_name = os.path.join(temp_localized_dir_name, i_jpg)
         cv2.imwrite(temp_localized_name, localized_frame)
-        localized_frame_ls.append(localized_frame)
         # if USE_AWS:
         #   localized_key = s3Key([*localized_dir_arg_ls, i_jpg])
         #   s3_client.upload_file(temp_localized_name, AWS_S3_BUCKET, localized_key)
@@ -143,10 +143,6 @@ async def receive_video(topic: str = Form(...), file: Union[UploadFile, str] = F
     #   xyxyn_key = s3Key(xyxyn_arg_ls)
     #   s3_client.upload_file(temp_xyxyn_name, AWS_S3_BUCKET, xyxyn_key)
     #   os.remove(temp_xyxyn_name)
-    return localized_frame_ls
-
-  localized_frame_ls = localizeFrames()
-  os.remove(temp_mp4_name)
 
   def restoreFrames():
     temp_restored_dir_name = tempName(["frame", "restored"])
@@ -164,84 +160,99 @@ async def receive_video(topic: str = Form(...), file: Union[UploadFile, str] = F
       i = temp_restored_basename.replace(".jpg", "")
       yield i, restored_frame
 
-  multitask_key_ls = ["moving", "smiling", "upright", "ec"]
-  multitask_df_dict = {key: [] for key in ["i", *multitask_key_ls]}
-  frame_ls = []
-  for batch_i_ls, batch_frame_ls in processRestoredFrames(restoreFrames()):
-    batch_frame_tensor = toTensor(batch_frame_ls).to(device)
-    multitask_pred = infer(multitask_model, batch_frame_tensor)
-    multitask_df_dict["i"].extend(batch_i_ls)
-    frame_ls.extend(batch_frame_ls)
-    for j, key in enumerate(multitask_key_ls):
-      multitask_df_dict[key].extend(multitask_pred[:, j])
-  multitask_df = pd.DataFrame(multitask_df_dict).set_index("i")
-  # if USE_AWS:
-  #   multitask_arg_ls = ["frame", "multitask.csv"]
-  #   temp_multitask_name = tempName(multitask_arg_ls)
-  #   multitask_df.to_csv(temp_multitask_name)
-  #   multitask_key = s3Key(multitask_arg_ls)
-  #   s3_client.upload_file(temp_multitask_name, AWS_S3_BUCKET, multitask_key)
-  #   os.remove(temp_multitask_name)
-  for key in multitask_key_ls:
-    Item[key] = {"N": str(multitask_df[key].mean())}
+  def predictFrames():
+    multitask_key_ls = ["moving", "smiling", "upright", "ec"]
+    multitask_df_dict = {key: [] for key in ["i", *multitask_key_ls]}
+    frame_ls = []
+    for batch_i_ls, batch_frame_ls in processRestoredFrames(restoreFrames()):
+      batch_frame_tensor = toTensor(batch_frame_ls).to(device)
+      multitask_pred = infer(multitask_model, batch_frame_tensor)
+      multitask_df_dict["i"].extend(batch_i_ls)
+      frame_ls.extend(batch_frame_ls)
+      for j, key in enumerate(multitask_key_ls):
+        multitask_df_dict[key].extend(multitask_pred[:, j])
+    multitask_df = pd.DataFrame(multitask_df_dict).set_index("i")
 
-  attire_df_dict = {
-      "i": multitask_df.index[FRAME_ATTIRE_MASK],
-      "attire": np.array(frame_ls)[FRAME_ATTIRE_MASK]
-  }
-  attire_frame_tensor = toTensor(attire_df_dict["attire"]).to(device)
-  attire_pred = infer(attire_model, attire_frame_tensor)
-  attire_df_dict["attire"] = attire_pred[:, 0]
-  attire_df = pd.DataFrame(attire_df_dict).set_index("i")
-  # if USE_AWS:
-  #   attire_arg_ls = ["frame", "attire.csv"]
-  #   temp_attire_name = tempName(attire_arg_ls)
-  #   attire_df.to_csv(temp_attire_name)
-  #   attire_key = s3Key(attire_arg_ls)
-  #   s3_client.upload_file(temp_attire_name, AWS_S3_BUCKET, attire_key)
-  #   os.remove(temp_attire_name)
-  attire_mode = bool(attire_df["attire"].mode()[0])
-  Item["professional_attire"] = {"BOOL": attire_mode}
+    # if USE_AWS:
+    #   multitask_arg_ls = ["frame", "multitask.csv"]
+    #   temp_multitask_name = tempName(multitask_arg_ls)
+    #   multitask_df.to_csv(temp_multitask_name)
+    #   multitask_key = s3Key(multitask_arg_ls)
+    #   s3_client.upload_file(temp_multitask_name, AWS_S3_BUCKET, multitask_key)
+    #   os.remove(temp_multitask_name)
+    for key in multitask_key_ls:
+      Item[key] = {"N": str(multitask_df[key].mean())}
 
-  speech_stats_key_ls = ["enthusiasm", "clarity"]
-  speech_stats_df_dict = {key: [] for key in ["i", *speech_stats_key_ls]}
-  for batch_i, batch_window in splitAudioBatch(splitAudio(temp_wav_name)):
-    batch_window_tensor = toTensor(batch_window)
-    speech_stats = infer(speech_stats_model, batch_window_tensor)
-    speech_stats_df_dict["i"].extend(batch_i)
-    for j, key in enumerate(speech_stats_key_ls):
-      speech_stats_df_dict[key].extend(speech_stats[:, j])
-  speech_stats_df = pd.DataFrame(speech_stats_df_dict).set_index("i")
-  # if USE_AWS:
-  #   speech_stats_arg_ls = ["audio", "stats.csv"]
-  #   temp_speech_stats_name = tempName(speech_stats_arg_ls)
-  #   speech_stats_df.to_csv(temp_speech_stats_name)
-  #   speech_stats_key = s3Key(speech_stats_arg_ls)
-  #   s3_client.upload_file(temp_speech_stats_name, AWS_S3_BUCKET, speech_stats_key)
-  #   os.remove(temp_speech_stats_name)
-  for key in speech_stats_key_ls:
-    Item_key = f"speech_{key}"
-    Item[Item_key] = {"N": str(speech_stats_df[key].mean())}
+    attire_df_dict = {
+        "i": multitask_df.index[FRAME_ATTIRE_MASK],
+        "attire": np.array(frame_ls)[FRAME_ATTIRE_MASK]
+    }
+    attire_frame_tensor = toTensor(attire_df_dict["attire"]).to(device)
+    attire_pred = infer(attire_model, attire_frame_tensor)
+    attire_df_dict["attire"] = attire_pred[:, 0]
+    attire_df = pd.DataFrame(attire_df_dict).set_index("i")
+    # if USE_AWS:
+    #   attire_arg_ls = ["frame", "attire.csv"]
+    #   temp_attire_name = tempName(attire_arg_ls)
+    #   attire_df.to_csv(temp_attire_name)
+    #   attire_key = s3Key(attire_arg_ls)
+    #   s3_client.upload_file(temp_attire_name, AWS_S3_BUCKET, attire_key)
+    #   os.remove(temp_attire_name)
+    attire_mode = bool(attire_df["attire"].mode()[0])
+    Item["professional_attire"] = {"BOOL": attire_mode}
 
-  def transcribeAudio():
+  def framesFn():
+    localizeFrames()
+    os.remove(temp_mp4_name)
+    predictFrames()
+
+  def predictSpeechStats():
+    speech_stats_key_ls = ["enthusiasm", "clarity"]
+    speech_stats_df_dict = {key: [] for key in ["i", *speech_stats_key_ls]}
+    for batch_i, batch_window in splitAudioBatch(splitAudio(temp_wav_name)):
+      batch_window_tensor = toTensor(batch_window)
+      speech_stats = infer(speech_stats_model, batch_window_tensor)
+      speech_stats_df_dict["i"].extend(batch_i)
+      for j, key in enumerate(speech_stats_key_ls):
+        speech_stats_df_dict[key].extend(speech_stats[:, j])
+    speech_stats_df = pd.DataFrame(speech_stats_df_dict).set_index("i")
+    # if USE_AWS:
+    #   speech_stats_arg_ls = ["audio", "stats.csv"]
+    #   temp_speech_stats_name = tempName(speech_stats_arg_ls)
+    #   speech_stats_df.to_csv(temp_speech_stats_name)
+    #   speech_stats_key = s3Key(speech_stats_arg_ls)
+    #   s3_client.upload_file(temp_speech_stats_name, AWS_S3_BUCKET, speech_stats_key)
+    #   os.remove(temp_speech_stats_name)
+    for key in speech_stats_key_ls:
+      Item_key = f"speech_{key}"
+      Item[Item_key] = {"N": str(speech_stats_df[key].mean())}
+
+  def predictPitch():
     pitch_arg_ls = ["pitch.txt"]
     temp_pitch_name = tempName(pitch_arg_ls)
     pitch = transcribe(temp_wav_name)
-    return pitch
+    Item["pitch"] = {"S": pitch}
+    try:
+      beholder_response = runBeholderFirst(topic, pitch)
+    except ValueError as e:
+      raise HTTPException(status_code=500, detail=str(e))
+    for key, value in beholder_response:
+      Item_key = f"beholder_{key}"
+      if key.endswith("_justification"):
+        continue
+        Item[Item_key] = {"S": value}
+      else:
+        Item[Item_key] = {"N": str(value)}
 
-  pitch = transcribeAudio()
-  Item["pitch"] = {"S": pitch}
-  try:
-    beholder_response = runBeholderFirst(topic, pitch)
-  except ValueError as e:
-    raise HTTPException(status_code=500, detail=str(e))
-  for key, value in beholder_response:
-    Item_key = f"beholder_{key}"
-    if key.endswith("_justification"):
-      continue
-      Item[Item_key] = {"S": value}
-    else:
-      Item[Item_key] = {"N": str(value)}
+  # Create a ThreadPoolExecutor to run the functions in parallel
+  with concurrent.futures.ThreadPoolExecutor() as executor:
+    # Submit the functions to the executor for parallel execution
+    frames_future = executor.submit(framesFn)
+    speech_stats_future = executor.submit(predictSpeechStats)
+    pitch_future = executor.submit(predictPitch)
+
+    # Wait for all futures to complete
+    concurrent.futures.wait([frames_future, speech_stats_future, pitch_future])
 
   print(Item)
   if USE_AWS:

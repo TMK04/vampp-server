9205097deed5f6232dffea694ca83a87896b64db
TMK04 <floer.learner.01@gmail.com>
2024-01-21 12:09:03 +0800

    install flash-attn

--- script_install_deps.sh
+++ script_install_deps.sh
@@ -7,3 +7,7 @@ pip install -r "$ROOT_DIR/requirements/install_deps.txt"
 ## CodeFormer
 
 ln -s "$ROOT_DIR/models/face_restorer/CodeFormer/facelib" "$ROOT_DIR/facelib"
+
+## Flash Attention
+
+pip install flash-attn --no-build-isolation

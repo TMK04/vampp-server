87444e57c6aca3e15f58966128ee7fbe572a6552
TMK04 <floer.learner.01@gmail.com>
2024-02-11 23:33:27 +0800

    refactor(exllamav2): move from requirements to submodule

--- script_install_deps.sh
+++ script_install_deps.sh
@@ -1,13 +1,18 @@
 # Install dependencies
 
+SERVER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 ## Main
 
-pip install -r "$ROOT_DIR/requirements/install_deps.txt"
+pip install -r "$SERVER_DIR/requirements/install_deps.txt"
+
+## ExllamaV2
+
+git submodule update --init --recursive
+cd "$SERVER_DIR/exllamav2"
+python setup.py install
+pip install flash-attn --no-build-isolation
 
 ## CodeFormer
 
-ln -s "$ROOT_DIR/models/face_restorer/CodeFormer/facelib" "$ROOT_DIR/facelib"
-
-## Flash Attention
-
-pip install flash-attn --no-build-isolation
+ln -s "$SERVER_DIR/models/face_restorer/CodeFormer/facelib" "$SERVER_DIR/facelib"

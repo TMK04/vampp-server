1eee68dd695f46e5d64858618d8aa45a4a6700ea
TMK04 <floer.learner.01@gmail.com>
2024-02-12 00:32:26 +0800

    refactor(transcribe): strip

--- models/transcriber/__init__.py
+++ models/transcriber/__init__.py
@@ -2,6 +2,6 @@ from .models import punctuation_restorer, transcriber
 
 
 def transcribe(wav_path: str):
-  raw_content = transcriber.transcribe([wav_path])[0]
-  restored_content = punctuation_restorer.add_punctuation_capitalization(raw_content)
-  return restored_content[0]
+  raw_content = transcriber.transcribe([wav_path])[0][0].strip()
+  restored_content = punctuation_restorer.add_punctuation_capitalization([raw_content])[0].strip()
+  return restored_content

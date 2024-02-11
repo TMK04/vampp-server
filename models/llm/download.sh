a70a83e5d24913e35b4ea7cc852c384f1e7918f3
TMK04 <floer.learner.01@gmail.com>
2024-02-11 23:48:52 +0800

    remove nested models dirs

--- models/llm/download.sh
+++ models/llm/download.sh
@@ -2,5 +2,5 @@ SERVER_LLM_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pw
 SERVER_LLM_MODELS_DIR="$SERVER_LLM_DIR/models"
 
 # Feel free to use other models
-mkdir "$SERVER_LLM_MODELS_DIR/$MODEL_LLM_AUTHOR"
+mkdir -p "$SERVER_LLM_MODELS_DIR/$MODEL_LLM_AUTHOR"
 mkdir "$SERVER_LLM_MODELS_DIR/$MODEL_LLM_AUTHOR/$MODEL_LLM_NAME" && huggingface-cli download "$MODEL_LLM_AUTHOR/$MODEL_LLM_NAME" --local-dir "$SERVER_LLM_MODELS_DIR/$MODEL_LLM_AUTHOR/$MODEL_LLM_NAME" --local-dir-use-symlinks False

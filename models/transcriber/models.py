f15f3add7db27901e5695c82411f673fbc71aa36
TMK04 <floer.learner.01@gmail.com>
2024-02-13 14:07:25 +0800

    Revert "refactor(punctuation_restorer): downgrade? to distilbert"
    This reverts commit 8f94ca7ffdf580bbdd1a4f00137d27146a5feefc.

--- models/transcriber/models.py
+++ models/transcriber/models.py
@@ -2,4 +2,4 @@ from nemo.collections.asr.models import EncDecRNNTBPEModel
 from nemo.collections.nlp.models import PunctuationCapitalizationModel
 
 transcriber = EncDecRNNTBPEModel.from_pretrained("nvidia/parakeet-tdt-1.1b")
-punctuation_restorer = PunctuationCapitalizationModel.from_pretrained("punctuation_en_distilbert")
+punctuation_restorer = PunctuationCapitalizationModel.from_pretrained("punctuation_en_bert")

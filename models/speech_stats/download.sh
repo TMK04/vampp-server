540f733f0163cd13c00dd1872a47b64f45ffe5c1
TMK04 <floer.learner.01@gmail.com>
2024-02-20 12:30:36 +0800

    refactor(speech_stats/pretrained): move from submodule to hugginface download

--- models/speech_stats/download.sh
+++ models/speech_stats/download.sh
@@ -1,4 +1,4 @@
 SERVER_SS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SERVER_SS_MODELS_DIR="$SERVER_SS_DIR/models"
 
-mkdir "$SERVER_SS_MODELS_DIR" && huggingface-cli download beholder-vampp/speech_stats --local-dir "$SERVER_SS_MODELS_DIR" --local-dir-use-symlink False
+mkdir "$SERVER_SS_DIR/pretrained" && huggingface-cli download audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim --local-dir "$SERVER_SS_DIR/pretrained" --local-dir-use-symlink False --exclude pytorch_model.bin
+mkdir "$SERVER_SS_DIR/models" && huggingface-cli download beholder-vampp/speech_stats --local-dir "$SERVER_SS_DIR/models" --local-dir-use-symlink False
